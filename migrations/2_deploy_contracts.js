b8ab49b0e6a2a23fbb72f210714adfa514f4766f
Davide <dcarboni@gmail.com>
2017-10-27 00:02:09 +0200

    minor mod

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,10 @@
 //var ConvertLib = artifacts.require("./ConvertLib.sol");
 //var MetaCoin = artifacts.require("./MetaCoin.sol");
-var TPLToken = artifacts.require("./TPLToken.sol");
+var TPLToken = artifacts.require("./SampleCrowdSale.sol");
 
 module.exports = function(deployer) {
 //  deployer.deploy(ConvertLib);
 //  deployer.link(ConvertLib, MetaCoin);
 //  deployer.deploy(MetaCoin);
-  deployer.deploy(TPLToken);
+    deployer.deploy(SampleCrowdSale);
 };
