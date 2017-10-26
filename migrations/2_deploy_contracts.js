7fc98d77950f352b514aa4265035a2f6cf484373
Davide <dcarboni@gmail.com>
2017-10-27 01:03:15 +0200

    still issue with gas when migrate on testrpc

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,10 +1,16 @@
 //var ConvertLib = artifacts.require("./ConvertLib.sol");
 //var MetaCoin = artifacts.require("./MetaCoin.sol");
-var TPLToken = artifacts.require("./SampleCrowdSale.sol");
+var SampleCrowdSale = artifacts.require("./SampleCrowdSale.sol");
+var SampleCrowdSaleToken = artifacts.require("./SampleCrowdSaleToken.sol");
+var Hello = artifacts.require("./Hello.sol");
 
 module.exports = function(deployer) {
 //  deployer.deploy(ConvertLib);
 //  deployer.link(ConvertLib, MetaCoin);
 //  deployer.deploy(MetaCoin);
-    deployer.deploy(SampleCrowdSale);
+    deployer.deploy(SampleCrowdSaleToken, {gas:1000000000});
+    deployer.deploy(SampleCrowdSale, {gas:1000000000});
+
+    
+    deployer.deploy(Hello);
 };
