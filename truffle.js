97e580cdf90913ddf8d52ad92b25f7ef02f7ea12
dadabit <dcarboni@gmail.com>
2017-11-22 23:19:49 +0100

    this version works with mainnet

--- truffle.js
+++ truffle.js
@@ -6,7 +6,19 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: "*" // Match any network id
-    },
+    },   
+
+    mainnet: {
+    host: "127.0.0.1", // Random IP for example purposes (do not use)
+    port: 8545,
+    network_id: 1,        // Ethereum test network
+    // optional config values:
+    gas: 4500000,
+    gasPrice: 1000000000,
+    // from - default address to use for any transaction Truffle makes during migrations
+    // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
+    //          - if specified, host and port are ignored.
+   },
    ropsten: {
     host: "127.0.0.1", // Random IP for example purposes (do not use)
     port: 8545,
